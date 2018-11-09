a56669bf8526bc5fa4a1c936c2ab2e31c5f62bb7
mwbouwkamp <mwbouwkamp@gmail.com>
2018-11-09 22:09:32 +0100

    Implement CaesarShiftCipherTest

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,89 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
 
+    private CaesarShiftCipher caesarShiftCipher;
+
+    @BeforeEach
+    public void init() {
+        caesarShiftCipher = new CaesarShiftCipher();
+    }
+
+    @Test
+    public void noShift() {
+        String message = "abc";
+        int shift = 0;
+        String expected = message;
+        String result = caesarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void Shift26() {
+        String message = "abc";
+        int shift = 26;
+        String expected = message;
+        String result = caesarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void ShiftPositive5NoCrossing() {
+        String message = "klm";
+        int shift = 5;
+        String expected = "pqr";
+        String result = caesarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void ShiftNegative5NoCrossing() {
+        String message = "klm";
+        int shift = -5;
+        String expected = "fgh";
+        String result = caesarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void ShiftPositive5Crossing() {
+        String message = "xyz";
+        int shift = 5;
+        String expected = "cde";
+        String result = caesarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void ShiftNegative5Crossing() {
+        String message = "abc";
+        int shift = -5;
+        String expected = "vwx";
+        String result = caesarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void invalidNoShift() {
+        String message = "!abc";
+        int shift = 0;
+        String expected = "invalid";
+        String result = caesarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+
+    }
+
+    @Test
+    public void invalidShiftPositive5() {
+        String message = "!abc";
+        int shift = 5;
+        String expected = "invalid";
+        String result = caesarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+
+    }
 }
